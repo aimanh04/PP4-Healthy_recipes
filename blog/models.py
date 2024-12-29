72caba875a720b3113f5f9257aac8dc57dcaa424
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-12-29 17:00:22 +0000

    Add recipepost model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+SERVINGS = [tuple([x,x]) for x in range(1,6)]
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class RecipePost(models.Model):
+    
+    """
+    Stores a single Recipe post entry related to :model:`auth.User`.
+    """
+
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe_posts"
+    )
+    # featured_image = CloudinaryField('image', default='placeholder')
+    prep_time = models.CharField(max_length=100)
+    cook_time = models.CharField(max_length=100)
+    serving = models.IntegerField(choices=SERVINGS)
+    # ingredient = models.ForeignKey(
+    #     Ingredient, on_delete=models.CASCADE, related_name="recipe_posts"
+    # )
+    instruction = models.TextField()
+    excerpt = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
--- healthyrecipes/settings.py
+++ healthyrecipes/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+ji9!kc@d#l#8c@jtb0f4cjb(@edp1ion(-0ed!u)-@**js6h'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -91,6 +91,12 @@ WSGI_APPLICATION = 'healthyrecipes.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+    "https://*.gitpod.io",
+]
 
 
 # Password validation
