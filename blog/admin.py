9017747128cf6d8733c7c1fd9efb0d2a690d5910
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-12-30 23:54:13 +0000

    Add comment model

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import RecipePost
+from .models import Recipe, Comment
 
 # Register your models here.
-admin.site.register(RecipePost)
+admin.site.register(Recipe)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-SERVINGS = [tuple([x,x]) for x in range(1,6)]
+SERVINGS = [tuple([x,x]) for x in range(1,7)]
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class RecipePost(models.Model):
+class Recipe(models.Model):
     
     """
-    Stores a single Recipe post entry related to :model:`auth.User`.
+    Stores a single Recipe post entered by user related to :model:`auth.User`.
     """
 
     title = models.CharField(max_length=200, unique=True)
@@ -18,13 +18,27 @@ class RecipePost(models.Model):
     # featured_image = CloudinaryField('image', default='placeholder')
     prep_time = models.CharField(max_length=100)
     cook_time = models.CharField(max_length=100)
-    serving = models.IntegerField(choices=SERVINGS)
-    # ingredient = models.ForeignKey(
-    #     Ingredient, on_delete=models.CASCADE, related_name="recipe_posts"
-    # )
+    servings = models.IntegerField(choices=SERVINGS)
+    ingredients = models.TextField(blank=True)
     instruction = models.TextField()
     excerpt = models.TextField(blank=True)
     notes = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    """
+    Stores a comment entered by a user related to :model:`auth.User`
+    and :model:`blog.Recipe`.
+    """
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
