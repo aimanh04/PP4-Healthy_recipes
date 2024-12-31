a5133224e48ca4bdd49005da69684813beb874ee
aimanh04 h <aimanhawaemanoh04@gmail.com>
2024-12-31 00:02:21 +0000

    Udate the blog models

--- blog/models.py
+++ blog/models.py
@@ -27,6 +27,11 @@ class Recipe(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on", "author"]
+    def __str__(self):
+        return f"{self.title} | recipe added by {self.author}"
+
 
 class Comment(models.Model):
     """
@@ -42,3 +47,8 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} comment left by {self.author}"
