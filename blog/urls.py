039cc78cf7b1cef5a365107ce8206bd1d4a80b86
aimanh04 <aimanhawaemanoh04@gmail.com>
2025-02-19 20:53:35 +0100

    Add: Edit functionality to the comments

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
-    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'), 
+    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Recipe
+from django.http import HttpResponseRedirect
+from .models import Recipe, Comment
 from .forms import CommentForm
 
 
@@ -61,3 +62,26 @@ def recipe_detail(request, slug):
         # {"recipe": recipe,
         #  "coder": "Matt Rudge"},
     )
+
+
+def comment_edit(request, slug, comment_id):
+    """
+    The view to edit comments in Recipe posts 
+    """
+    if request.method == "POST":
+
+        queryset = Recipe.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.success(request, 'Your comment has been Updated and is now awaiting approval!')
+        else:
+            messages.error(request, 'There was an error updating your comment!')
+
+    return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
